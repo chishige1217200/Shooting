81931074ed72eb72c9ae8d8ae638431db8b653ea
chishige1217200 <daichicchi01@gmail.com>
2020-11-09 20:25:10 +0900

    [add] Bullet.cs

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+        //Shot(5f, 50f);
+    }
+
+    public void Shot(float speed, float radian)
+    {
+        float force_x;
+        float force_y;
+        radian = radian * (float)(Math.PI / 180); //ラジアン(pi)に変換
+        force_x = speed * (float)Math.Cos(radian); //x軸方向の力を計算
+        force_y = speed * (float)Math.Sin(radian); //z軸方向の力を計算
+        rb.AddForce(force_x, force_y, 0, ForceMode.VelocityChange); //瞬間的に弾に力を加える(質量無視)
+    }
+
+    void OnCollisionEnter()
+    {
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/Tarrot.cs
+++ Assets/Scripts/Tarrot.cs
@@ -1,17 +1,33 @@
 using System;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class Tarrot : MonoBehaviour {
     public GameObject center; //円運動の中心になるObject
-    float speed = 60; //円運動速度
+    public GameObject bulletPrefab; //弾オブジェクトのプレハブ
+    private GameObject clone;       //Instantiateされたオブジェクト
+    private Bullet bulletScript;    //スクリプトのインスタンス
+    private float speed = 60; //円運動速度
+    private bool shotFlag = false; //発射中はtrue
 
     void Update () {
         transform.RotateAround (center.transform.position, transform.forward, speed * Time.deltaTime); //回転
 
         if (Input.GetKey (KeyCode.LeftArrow) && speed < 0 || Input.GetKey (KeyCode.RightArrow) && speed > 0) speed = -speed;
         if (Input.GetKey (KeyCode.Return)) {
-
+            if(!shotFlag) MakeBullet(5f);
         }
+    }
+
+    public async void MakeBullet(float speed)
+    {
+        shotFlag = true;
+        clone = Instantiate(bulletPrefab, this.transform.position, Quaternion.identity);
+        bulletScript = clone.GetComponent<Bullet>();
+        bulletScript.Shot(speed, 0);
+
+        await Task.Delay(100);
+        shotFlag = false;
     }
 
 }
