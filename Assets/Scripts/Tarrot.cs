86fa4637aff734133716d5b28ca0a2cd0e1c51fc
chishige1217200 <daichicchi01@gmail.com>
2020-11-19 22:26:10 +0900

    [add] 発射方向を動的に指定

--- Assets/Scripts/Tarrot.cs
+++ Assets/Scripts/Tarrot.cs
@@ -11,20 +11,22 @@ public class Tarrot : MonoBehaviour {
     private bool shotFlag = false; //発射中はtrue
 
     void Update () {
+        float z;
         transform.RotateAround (center.transform.position, transform.forward, speed * Time.deltaTime); //回転
-
+        z = this.transform.rotation.z * 180;
+        Debug.Log(z);
         if (Input.GetKey (KeyCode.LeftArrow) && speed < 0 || Input.GetKey (KeyCode.RightArrow) && speed > 0) speed = -speed;
         if (Input.GetKey (KeyCode.Return)) {
-            if(!shotFlag) MakeBullet(5f);
+            if(!shotFlag) MakeBullet(5f, z);
         }
     }
 
-    public async void MakeBullet(float speed)
+    public async void MakeBullet(float speed, float radian)
     {
         shotFlag = true;
         clone = Instantiate(bulletPrefab, this.transform.position, Quaternion.identity);
         bulletScript = clone.GetComponent<Bullet>();
-        bulletScript.Shot(speed, 0);
+        bulletScript.Shot(speed, radian);
 
         await Task.Delay(100);
         shotFlag = false;
